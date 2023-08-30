44adef8ff38b4a34d3a4d73ff34b00806cf241be
RudyCutz <rudygcs@gmail.com>
2023-08-30 12:17:54 -0600

    primer commit

---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+variable1 = 5
+variable2 = 6
+ 
+suma = variable1 + variable2
+ 
+print("La suma de ",variable1,"+",variable2,"=",suma)
---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+a = 7
+b = 5
+ 
+if( a<b ):
+    print("A es menor que B")
+else:
+    print("B es menor que A")
